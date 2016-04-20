784201eb6e016a4634567d9d780e40392ed40992
Brendan Whitfield <brendanw@windworksdesign.com>
2016-04-20 19:49:01 -0400

    removed remnant 3D code

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,21 +1,11 @@
 #pragma once
 
-#include <opencv2/calib3d.hpp>
-
 #include "ofMain.h"
 #include "beamCamera.h"
 #include "ofxMidi.h"
 
-
-using namespace cv;
-
-//TODO: make this more dynamic
-#define CALIB_BEAMS 4
-#define CALIB_HEIGHTS 3
-
 //UI
 #define THRESHOLD_INCREMENT 2
-const int CALIB_KEYS[] = { 'z', 'x', 'c', 'v', 'a', 's', 'd', 'f', 'q', 'w', 'e', 'r' };
 
 class ofApp : public ofBaseApp{
 	public:
