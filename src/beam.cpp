20db35a6d3d6175a1b8691082f9519bfa83a3e4d
Brendan Whitfield <brendanw@windworksdesign.com>
2016-05-04 21:34:02 -0400

    don't do this. it doesn't send note OFF commands

--- src/beam.cpp
+++ src/beam.cpp
@@ -43,10 +43,6 @@ void Beam::draw(vector<Hand> hands)
 
 void Beam::update(vector<Hand> hands, ofxMidiOut& midi_out)
 {
-    //return early if there's nothing to do
-    if(hands.size() == 0)
-        return;
-
     BeamRegion regions[SCALE_SIZE];
     float max_vel = 0.0;
 
